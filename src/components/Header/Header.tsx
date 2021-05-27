69fe0344e5d4e0394a3a5781ca4c29d54a6d0465
Vantol Bennett <vvantol2000@icloud.com>
2021-05-27 00:00:51 -0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <nav className="navbar navbar-light bg-light starbucks-nav">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <div className="navbar-brand">
                     <img src="" alt="" width="30" height="24"
                          className="d-inline-block align-text-top"/>
                         Package Request
-                </a>
+                </div>
             </div>
         </nav>
     );
