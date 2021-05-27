386980b49e11a8f30cb3f5e7c2b400f81fca81ff
Vantol Bennett <vvantol2000@icloud.com>
2021-05-27 00:00:55 -0500

    Update SearchList.tsx

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -20,19 +20,19 @@ const SearchList = ({data, error, loading}: SearchListProps) => {
                         <div className="col-lg-4 mb-lg-0 mb-4 p-4" key={repo.name}>
                             <div className="card">
                                 <div className="card-header p-0 mx-3 mt-3 position-relative z-index-1">
-                                    <a href="#" className="d-block blur-shadow-image">
+                                    <div className="d-block blur-shadow-image">
                                         <img src="https://pbs.twimg.com/media/ETPwXGaXsAA2qTD.png" className="img-fluid border-radius-md"
                                              alt={repo.name}/>
-                                    </a>
+                                    </div>
                                     {/*    <div className="colored-shadow"*/}
                                     {/*         style="background-image: url(&quot;../../assets/img/annie-spratt.jpg&quot;);"></div>*/}
                                 </div>
                                 <div className="card-body">
                                 <span
-                                    className="text-gradient text-warning text-uppercase text-xs font-weight-bold">{repo.scope}</span>
-                                    <a href="#" className="text-darker card-title mt-3 h5 d-block">
+                                    className="text-gradient text-warning text-uppercase text-xs font-weight-bold">{repo.name}</span>
+                                    <div  className="text-darker card-title mt-3 h5 d-block">
                                         Homepage: {repo.homepage}
-                                    </a>
+                                    </div>
                                     <p className="card-description mb-4">
                                         {repo.description}
                                     </p>
